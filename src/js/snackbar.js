1084f899ebbab3158a5a9311b83e134436042958
Agnieszka Szczepanik <agnieszkaszczepanik93@gmail.com>
2024-10-03 11:55:44 +0200

    snack

---
+++ src/js/snackbar.js
@@ -0,0 +1,33 @@
+// Opisany w dokumentacji
+import iziToast from 'izitoast';
+// Kolejny import stylów
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = form.elements.delay.value;
+  const state = form.elements.state.value;
+
+  createPromise(delay, state)
+    .then(delay => {
+      iziToast.success({ message: `✅ Fulfilled promise in ${delay}ms` });
+    })
+    .catch(delay => {
+      iziToast.warning({ message: `❌ Rejected promise in ${delay}ms` });
+    });
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === `fulfilled`) {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
